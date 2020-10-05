e1f0422023253856caa1533253201be3052d9d9e
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-10-05 17:52:48 +0200

    Added next.config.js file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+console.log(process.env.GREETING)
+
 export default function Home() {
   return (
     <div className={styles.container}>
